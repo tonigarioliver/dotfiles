f2847a98f2ffb5ab19e3e9e8454ed4d2f71b563a
ToniGari <104216546+tonigarioliver@users.noreply.github.com>
2024-02-24 11:38:37 +0100

    Update setup.sh

--- setup/setup.sh
+++ setup/setup.sh
@@ -42,7 +42,6 @@ if ! is_installed docker-ce; then
     echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo "$UBUNTU_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
     sudo apt-get update
     sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-    sudo docker run hello-world
     sudo groupadd docker
     sudo usermod -aG docker $USER
     newgrp docker

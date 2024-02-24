27d3f71350a61d1dfcbaef853a16656727d78e3a
Toni Gari <tonietmoscari@gmail.com>
2024-02-24 11:27:31 +0100

    Adding work env

--- run.sh
+++ run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+# Función para inicializar un repositorio Git y hacer un commit
+initialize_git() {
+    echo "Iniciando repositorio Git..."
+    git init
+    git add .
+    git commit -m "Initial commit"
+    echo "Repositorio Git iniciado y cambios commitados."
+}
+
+# Preguntar al usuario si es una instalación limpia
+read -p "¿It's a clean isntall? (Y/n): " clean_install
+if [[ $clean_install =~ ^[Yy]$ ]]; then
+    initialize_git
+fi
+
 # Cambiar al directorio del script
 cd "$HOME/dotfiles/setup"
 
